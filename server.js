4706731769e9da092c05b9006bdb6956232053f8
aditya-1123 <gaikwadaditya1123@gmail.com>
2024-11-22 11:45:44 +0530

    created the server and schemas for user and post

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./db');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
+connectDB();
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+ 
+module.exports = app;
